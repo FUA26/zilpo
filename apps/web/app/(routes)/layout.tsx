9a258fb9ba524be7ea442ac87d0e5a9e123f7b5d
Fatih UA <fatihulil@gmail.com>
2026-04-14 21:29:59 +0700

    fix:delete layout

--- apps/web/app/(routes)/layout.tsx
+++
@@ -1,6 +0,0 @@
-import type { ReactNode } from "react";
-import { ProductShell } from "../../components/shell/product-shell";
-
-export default function RoutesLayout({ children }: { children: ReactNode }) {
-  return <ProductShell>{children}</ProductShell>;
-}
